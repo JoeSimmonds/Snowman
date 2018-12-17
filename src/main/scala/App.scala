2ca7921ac988c9dc618a5273086977e902540c73
Joe Simmonds <joe.simmonds@restore.eu>
2018-12-17 23:04:01 +0000

    removed unecessary stuff

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,14 +1,6 @@
 import DrivableEvidence._
 
-import scala.util.Random
-
 object App {
-  def fillGaps(map: Map[Int, Int], max: Int, cur: Int) : Map[Int, Int] = {
-    if (cur > max) map
-    else if (map.contains(cur)) fillGaps(map, max, cur+1)
-    else fillGaps(map + (cur -> 0), max, cur+1)
-
-  }
 
   def run[T](init : () => T, count : Int)(implicit drivableEvidence: DrivableEvidence[T]) = {
     (1 to count)
